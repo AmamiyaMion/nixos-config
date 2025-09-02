1daadcb9e3f6ac38397b69faabace912e124e630
Amamiya Mion <50908468+AmamiyaMion@users.noreply.github.com>
2025-09-02 12:41:07 +0800

    obs

--- home/software.nix
+++ home/software.nix
@@ -12,6 +12,16 @@
   programs.zoxide.enable = true;
   programs.zoxide.enableBashIntegration= true;
 
+  # OBS Studio
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
+
   home.packages = with pkgs; [
     gnome-tweaks
     thunderbird
--- system/software.nix
+++ system/software.nix
@@ -71,6 +71,15 @@
   # ratbagd (for piper)
   services.ratbagd.enable = true;
 
+  # For OBS Studio Virtual Camera: v2l4loopback kernel module
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
